1a0295c994a8dee75f7ef083ebc3c5a0fde2d4ca
arcteg <sas.arcteg@gmail.com>
2022-03-29 16:38:34 +0300

    Conditions and IndexError fix

--- lab3.py
+++ lab3.py
@@ -1,10 +1,21 @@
 text = input("Введите строку: ")
-s = text.split('*')
-max_word = str(len(max(s, key=len)))
-max_string = max(s, key=len)
-print("Наибольшая подстрока: " + max_string)
-print("Длина наибольшей подстроки: " + max_word)
+max_stars = 0
+b = 0
+print(len(text))
+for i in range(len(text) - 1):
+    if text[i] == "*":
+        b = i + 1
+        count = 0
+        r = ""
+        while text[b] != "*" and b+1 != len(text):
+            count += 1
+            b += 1
+            r = text[b]
+        if count > max_stars and r == "*":
+            max_stars = count
+print(max_stars)
 t = "l"
+print("Наибольшая подстрока:", max_stars)
 for i in range(len(text)):
     if text[i] == "*":
         print(text[i], end="")
