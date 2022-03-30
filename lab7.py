49d77c915f0c995b77add854366a5c077cd20984
arcteg <sas.arcteg@gmail.com>
2022-03-31 01:44:41 +0300

    Dict reverse and minor improvements

--- lab7.py
+++ lab7.py
@@ -1,71 +1,84 @@
-dictionary = {"Компьютер": "Computer",
-              "Клавиатура": "Keyboard",
-              "Наушники": "Headphones",
-              "Земля": "Earth",
-              "Солнце": "Sun",
-              "Воздух": "Air",
-              "Вода": "Water",
-              "Огонь": "Fire",
-              "Окно": "Window",
-              "Ручка": "Pen"
+dictionary = {"Computer": "Компьютер",
+              "Keyboard": "Клавиатура",
+              "Can": ["Мочь", "Банка"],
+              "Earth": "Земля",
+              "Sun": "Солнце",
+              "Air": "Воздух",
+              "Box": "Ящик",
+              "Fire": "Огонь",
+              "Right": ["Право", "Прав"],
+              "Pen": "Ручка"
               }
+print(dictionary)
 
 
 #  добавление пары
-def add_pair(rus, eng):
-    dictionary[rus] = eng
+def add_pair(eng, rus):
+    dictionary[eng] = rus
+add_pair('Put', ["Ставить", "помещать"])
+print(dictionary)
 
 
 #  удаление
-def del_pair(rus):
-    del dictionary[rus]
+def del_pair(word):
+    if word in dictionary.keys():
+        del dictionary[word]
+del_pair("Can")
+print(dictionary)
 
 
 #  проверка на вхождение
 def check_pair(word):
-    if word in dictionary:
+    if word in dictionary or word in dictionary.values():
         return True
     else:
         return False
+print(check_pair("Земля"))
 
 
 #  вывод англ слов короче 5 символов
 def prnt_wrds():
-    for i in dictionary.values():
+    for i in dictionary.keys():
         if len(i) < 5:
             print(i)
+prnt_wrds()
 
 
 #  очистка словаря
 def clear_dict():
     dictionary.clear()
-
+#  в самом конце
 
 #  сортировка словаря
-def sort_dict():
-    sorted_dict = sorted(dictionary.items())
-    return sorted_dict
+def sort_dict(dt):
+    dictionary = dict(sorted(dt.items()))
+    return dictionary
+print(sort_dict(dictionary))
 
 
 #  вывод всего словаря
-def prnt_dctnry():
-    print(dictionary)
+def prnt_dctnry(dict):
+    for i in dict:
+        print(i, dict[i])
+    #  print(dictionary)
+prnt_dctnry(dictionary)
 
 
 #  изменение пары
 def chng_pair(old_key):
-    old_value = dictionary.get(old_key)
-    rus = list(dictionary.keys())
-    eng = list(dictionary.values())
-    old_key_index = rus.index(old_key)
-    old_value_index = eng.index(old_value)
     new_key = input("Введите новый ключ: ")
-    new_value = input("Введите новое значение: ")
-    rus[old_key_index] = new_key
-    eng[old_value_index] = new_value
-    d = dict(zip(rus, eng))
+    new_value = input("Введите новое значение: ").split()
+    dictionary[new_key] = dictionary.pop(old_key)
+    dictionary[new_key] = new_value
+chng_pair("Sun")
+print(dictionary)
 
 
 # смена ключа и значения местами
 def reverse(key):
     dictionary[dictionary.pop(key)] = key
+reverse("Air")
+print(dictionary)
+
+clear_dict()
+print(dictionary)
